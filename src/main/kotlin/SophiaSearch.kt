0e6f032a035e9742d2e59baf313db28bd41f2c8a
javacook <javacook@gmx.de>
2018-02-03 13:25:05 +0100

    Beautyfied

--- src/main/kotlin/SophiaSearch.kt
+++ src/main/kotlin/SophiaSearch.kt
@@ -1,17 +1,24 @@
 package de.kotlincook.sophiasearch
 
 /**
- * A wrapper around a string in order to compare its normed
+ * A wrapper around a string in order to compare its normalized form
  */
-class NormelizedString(val str: String) : Comparable<NormelizedString> {
-    val normalized = str.normalize()
-    override fun compareTo(other: NormelizedString): Int {
-        return normalized.compareTo(other.normalized)
+class NormalizedString(val str: String) : Comparable<NormalizedString> {
+    val value = str.normalize()
+    override fun compareTo(other: NormalizedString): Int {
+        return value.compareTo(other.value)
     }
 }
 
-class SophiSearch(val navSet: Collection<NormelizedString>) : Completable {
+class SophiSearch(val navSet: Collection<NormalizedString>) : Completable {
 
+    /**
+     * Returns the search result of <code>input</code>, e.g. a collection of
+     * words that match <code>input</code>. The result is ordered
+     * by the similarity with <code>input</code> at the 1st level and
+     * alphabetical at the 2nd level.
+     * @see CrumbIndexResult
+     */
     override fun complete(input: String): Collection<String> {
         // TODO: Please fill in here your code...
         return navSet.map { it.str }

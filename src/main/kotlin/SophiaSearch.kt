849aad5fc5b1581e88a34a03d10caad4bad436f5
javacook <javacook@gmx.de>
2018-02-04 07:09:44 +0100

    Comment

--- src/main/kotlin/SophiaSearch.kt
+++ src/main/kotlin/SophiaSearch.kt
@@ -20,6 +20,13 @@ class WrappedNormedString(val normalizedString: NormalizedString,
 
 class SophiSearch(val navSet: Collection<NormalizedString>) : Completable {
 
+    /**
+     * Returns the search result of <code>input</code>, e.g. a collection of
+     * words that match <code>input</code>. The result is ordered
+     * by the similarity with <code>input</code> at the 1st level and
+     * alphabetical at the 2nd level.
+     * @see CrumbIndexResult
+     */
     override fun complete(input: String): Collection<String> {
             val normed = input.normalized()
             var result: List<String> = ArrayList()
